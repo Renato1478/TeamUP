af2dde83c2c1c36b6c60e68e8a7a478203eecedc
Renato1478 <renatoperesdesouza@hotmail.com>
2020-02-18 19:39:01 -0300

    login page semi-builded

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import logo from './assets/logo.svg';
+
+import api from './services/api';
 
 function App() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(event) {
+
+    event.preventDefault();
+    
+    const response = await api.post('/login', {
+      email,
+      password
+    });
+
+    const {token} = response.data;
+    const {_id} = response.data.user;
+
+    localStorage.setItem('user', _id, token);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+
+      <img className="logo" src={ logo } alt='TeamUP' width='200px' height='200px'/>
+
+      <div className="content">
+
+        <form onSubmit={handleSubmit}>
+
+          <p>
+            Join to teams to evolve your gameplay
+          </p>
+
+          <label htmlFor="email">E-MAIL</label>
+          <input
+            type="email"
+            id="email" 
+            placeholder="Your better e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <label htmlFor="password">PASSWORD</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={event => setPassword(event.target.value)}
+          />
+
+          <button>LOGIN</button>
+
+        </form>
+
+      </div>
     </div>
   );
 }
