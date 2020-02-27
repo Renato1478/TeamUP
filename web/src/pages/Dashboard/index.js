09e15a122ba741573c26c957204fac2509651acd
Renato1478 <renatoperesdesouza@hotmail.com>
2020-02-27 17:01:32 -0300

    create team function

--- web/src/pages/Dashboard/index.js
+++ web/src/pages/Dashboard/index.js
@@ -1,41 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import {Link} from 'react-router-dom';
 
 import './styles.css';
 
-
+import logo from '../../assets/logo.svg';
 
 export default function Dashboard() {
 
     const [teams, setTeams] = useState([]);
 
     useEffect(() => {
-        async function loadSpots() {
-            const user_id = localStorage.getItem('user');
-            const response = await api.get('/teams');
+        async function loadTeams() {
+            const response = await api.get('/dashboard');
             
-            setTeams(response.data);
+            setTeams(response.data.teams);
         }
 
-        loadSpots();
+        loadTeams();
     }, []);
 
-    return (<>
-        <ul className="team-list">
-            {teams.map(team => (
-                <li key={team._id}>
-                    <strong>{team.name}</strong>
-                    <div>{team.game}</div>
-                    <span>{team.location}</span>
-                    
-                    <Link to='/team'>
-                        <button>PARTICIPATE</button>
-                    </Link>
-                    
-                </li>
-            ))}
-        </ul>
-    </>)
+    function goToTeamPage(teamId) {
+        localStorage.setItem('team_id', teamId);
+    }
 
+    return (<>
+        <img className="logo" src={ logo } alt='TeamUP' width='200px' height='200px'/>
+
+        <div className='team-table'>
+            <ul className="team-list">
+                {teams.map(team => (
+                    <Link style={{textDecoration: 'none'}} to='/team' key={team._id} onClick={event => goToTeamPage(team._id)}>
+                        <li className=''>
+                            <strong>{team.name}</strong>
+                            <div>{team.game}</div>
+                            <span>{team.location}</span>
+                        </li>
+                    </Link>
+                ))}
+            </ul>
+            <Link style={{textDecoration: 'none'}} to='/create_team'><button className='btn-red alignRight' style={{width: 330}}>Create a Team</button></Link>
+        </div>
+    </>)
 };
--- web/src/routes.js
+++ web/src/routes.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
 import Team from './pages/Team';
+import CreateTeam from './pages/CreateTeam';
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Switch>
+            <Switch>  
                 <Route path='/login' exact component={Login} />
-                <Route path='/dashboard' component={Dashboard} />
-                <Route path='/team' component={Team} />
+                <Route path='/dashboard' exact component={Dashboard} />
+                <Route path='/team' exact component={Team} />
+                <Route path='/create_team' exact component={CreateTeam} />
+                
+                <Redirect from='*' to='/dashboard' />
             </Switch>
         </BrowserRouter>
     )
